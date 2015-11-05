69e28171c0d3ee37d77c49d480ba9a69d14a6d7b
RyanG <decatf@gmail.com>
2015-11-05 12:50:11 -0500

    libc: non-neon cortex-a9

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -1,16 +1,34 @@
 libc_bionic_src_files_arm += \
-    arch-arm/cortex-a9/bionic/memcpy.S \
-    arch-arm/cortex-a9/bionic/memset.S \
-    arch-arm/cortex-a9/bionic/stpcpy.S \
     arch-arm/cortex-a9/bionic/strcat.S \
-    arch-arm/cortex-a9/bionic/__strcat_chk.S \
     arch-arm/cortex-a9/bionic/strcmp.S \
-    arch-arm/cortex-a9/bionic/strcpy.S \
-    arch-arm/cortex-a9/bionic/__strcpy_chk.S \
-    arch-arm/cortex-a9/bionic/strlen.S \
+    arch-arm/cortex-a9/bionic/stpcpy.S \
+    arch-arm/cortex-a9/bionic/strcpy.S
 
 libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/memcmp.S \
 
+ifeq ($(strip $(ARCH_ARM_HAVE_NEON)),true)
+
+libc_bionic_src_files_arm += \
+    arch-arm/cortex-a9/bionic/memcpy.S \
+    arch-arm/cortex-a9/bionic/memset.S \
+    arch-arm/cortex-a9/bionic/__strcat_chk.S \
+    arch-arm/cortex-a9/bionic/__strcpy_chk.S \
+    arch-arm/cortex-a9/bionic/strlen.S \
+
 libc_bionic_src_files_arm += \
     arch-arm/denver/bionic/memmove.S \
+
+else
+
+libc_bionic_src_files_arm += \
+    arch-arm/generic/bionic/memcpy.S \
+    arch-arm/generic/bionic/memset.S \
+    bionic/__strcat_chk.cpp \
+    bionic/__strcpy_chk.cpp \
+    arch-arm/cortex-a15/bionic/strlen.S \
+
+libc_bionic_src_files_arm += \
+    upstream-openbsd/lib/libc/string/memmove.c \
+
+endif
